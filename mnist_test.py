407ea2109fa26271db6788d2fcf8e17ed9441df7
gonzalo <gonzalolopecc@gmail.com>
2022-07-12 00:14:21 +0200

    Achieved 0.94 acc in finall big test

--- mnist_test.py
+++ mnist_test.py
@@ -24,18 +24,18 @@ second_image_shape = (image_shape[0]-first_filter_shape[0]+1,image_shape[1]-firs
 first_dense_inputs = (second_image_shape[0]-second_filter_shape[0]+1)*(second_image_shape[1]-second_filter_shape[1]+1) # 24*24 = 576
 cost_function: CostFunction = CategoricalCrossEntropy()
 layers: list[Layer] = [
-    Convolutional2D(ReLu,Adam,num_filters,first_filter_shape,image_shape,learning_rate=1e-5),
-    Dense(ReLu,Adam, 26*26*num_filters, 100, learning_rate=1e-3),
-    Dense(SoftMax,Adam,100,10,learning_rate=1e-3 )
+    Convolutional2D(ReLu,Adam,num_filters,first_filter_shape,image_shape,learning_rate=1e-4),
+    Dense(ReLu,Adam, 26*26*num_filters, 100, learning_rate=1e-4),
+    Dense(SoftMax,Adam,100,10,learning_rate=1e-4 )
 ]
 net = NeuralNetwork(layers,cost_function)
 
 batch_size = 10
-number_of_batches_per_epoch = 10
+number_of_batches_per_epoch = 50
 number_of_epochs = 100
 train_index = 0
 test_index = 0
-test_size = 10
+test_size = 20
 all_errors = []
 all_accuracy = []
 for epoch_number in range(number_of_epochs):
@@ -61,8 +61,22 @@ for epoch_number in range(number_of_epochs):
     for i, out in enumerate(output): 
         if np.argmax(out) == np.argmax(net_test_y[i]):
             acc += 1
-    all_accuracy.append(acc/10)
+    all_accuracy.append(acc/test_size)
     test_index += test_size 
+
+# Big Test of final verison
+b_test_size = 100
+acc = 0
+for i in range(b_test_size):
+    test_ind: int = np.random.choice(test_x.shape[0])
+    t_x = test_x[test_ind].reshape((1,*test_x[test_ind].shape))
+    t_y = test_y[test_ind]
+    output = net.feed_forward(t_x)
+    if np.argmax(output) == t_y:
+        acc += 1
+
+print(f'Big test acc: {acc/b_test_size}')
+
 
 print(f'Average acc:{np.mean(all_accuracy)}, Average err:{np.mean(all_errors)}')
 pyplot.plot(list(range(number_of_epochs)), all_errors, label='errors')
